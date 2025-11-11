cf22b6f0d1989adc516529142e33e33b47a34d08
anish2210 <anishjaiswal1220@gmail.com>
2025-11-11 13:31:47 +0530

    staged chages

--- client/src/components/landing/Banner.jsx
+++ client/src/components/landing/Banner.jsx
@@ -1,11 +1,25 @@
 import React from 'react'
+import logo from "../../assets/Logo/BitWisdom_Secondary_Logomark_Registered.png";
 
 const Banner = () => {
   return (
-    <div className='bg-black text-white p-4 flex space-x-*'>
-      <div>social</div>
-      <div>logo</div>
-      <div>others</div>
+    <div className="bg-black text-white flex justify-between items-center px-28">
+      <div className="flex gap-8">
+        <ul>
+          
+        </ul>
+        <ul>instagram</ul>
+        <ul>twitter</ul>
+        <ul>picsart</ul>
+      </div>
+      <div className="w-30 h-18 flex items-center">
+        <img src={logo} />
+      </div>
+      <div className="flex gap-4">
+        <ul>search</ul>
+        <div>|</div>
+        <ul>Language</ul>
+      </div>
     </div>
   );
 }
